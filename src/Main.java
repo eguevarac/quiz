e90519c648e47dd2e647dfd5349910f2105dc1dc
eguevarac <eguevarac2223@politecnics.barcelona>
2023-05-11 09:42:08 +0200

    trabajando con los ficheros

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,10 @@ public class Main {
             boolean lose = false;
             do {
                 System.out.println("\nPregunta número " + (questionIndex + 1) + ":");
-                System.out.println(questionsToPlay[questionIndex]);
+                //Tendría que hacer por aquí el chequeo de salto de línea, creo
+
+                formulatingQuestion(questionsToPlay[questionIndex]);
+
                 String answer = checkingYesOrNot(answersToPlay, questionIndex, questionsToPlay);
                 if (answer.equals(answersToPlay[questionIndex])) {
                     System.out.println("La respuesta es correcta!!\n");
@@ -105,6 +108,18 @@ public class Main {
 
     }
 
+    private static void formulatingQuestion(String questionToSplit) {
+
+
+
+        String[] splited = questionToSplit.split("@");
+        for (String s :
+                splited) {
+            System.out.println(s);
+        }
+
+    }
+
     private static void addingScore(Player player) {
 
         String playerScore = player.toString();
@@ -123,34 +138,7 @@ public class Main {
     private static void showingScore() {
         System.out.println("\nVEAMOS LAS ESTADÍSTICAS DE TODOS LOS JUGADORES:\n");
 
-        //para leer el archivo entero
-
-        /*
-        Path path = Paths.get("src/resources/scores.txt");
-        try {
-            List<String>linesFile = Files.readAllLines(path);
-
-            //con join podemos juntar todos los elementos de una matriz (indicando el separador que queramos)
-            String linesTogether = String.join("\n",linesFile);
-
-            //así muestra el texto a saco
-            System.out.println(linesFile);
-
-            //muestra el texto de dentro del String
-            System.out.println(linesTogether);
-
-        } catch (IOException e) {
-            System.out.println("ERROR BUSCANDO EL FICHERO DE TEXTO");
-        }
-
-         */
-
-
-
-
         //para leer el archivo líea a línea
-
-
         String fileName = "src/resources/scores.txt";
         BufferedReader br;
         String line;
@@ -343,171 +331,63 @@ public class Main {
     }
 
     private static void questionsBox(String[][] totalQuestions) {
-        totalQuestions[0][0] = "BD: Los tres grupos de instrucciones en SQL son  el DDL, el DML y el...?\n- DEL\n- DIL\n- DCL";
-        totalQuestions[0][1] = "BD: Las bases de datos se emplean para guardar información?";
-        totalQuestions[0][2] = "BD: En el modelo Entidad-Relación, cada uno de los recuadros equivale a una entidad?";
-        totalQuestions[0][3] = "BD: En el modelo Entidad-Relación, cada uno de los óvalos equivale a una relación?";
-        totalQuestions[0][4] = "BD: En el modelo Entidad-Relación, puede una relación tener atributos?";
-        totalQuestions[0][5] = "BD: Los atributos son las filas en las tablas de las bases de datos?";
-        totalQuestions[0][6] = "BD: Según la regla de integridad referencial, puede tener una clave foránea un valor null?";
-        totalQuestions[0][7] = "BD: Puede una clave primaria tener un valor null según la regla de integridad de entidad?";
-        totalQuestions[0][8] = "BD: En SQL, qué significan las siglas DDL?\n- Data Domination language\n- Data Definition Language\n- Data Direction Language\n- Diciembre Duerme Largo";
-        totalQuestions[0][9] = "BD: En SQL, qué significan las siglas DCL?\n- Data Class language\n- Donde Como luego\n- Data Control Language\n- Data Christmas Language";
-        totalQuestions[0][10] = "BD: En SQL, qué significan las siglas DML?\n- Dame mas libros\n- Data Manipulation Language\n- Data Massive Language\n- Data Movement Language";
-        totalQuestions[0][11] = "BD: La cardinalidad N:M siempre genera una nueva tabla?";
-        totalQuestions[0][12] = "BD: En SQL, las instrucciones CREATE, DROP y ALTER pertenecen al grupo de instrucciones del DML?";
-        totalQuestions[0][13] = "BD: En SQL, el comando SELECT * FROM 'nombre_tabla' nos muestra todos los elementos de la tabla mencionada?";
-        totalQuestions[0][14] = "BD: Qué quieren decir las siglas SQL?\n- Standard Query Language\n- Structured Query Language\n- Sin Queso Longanizas";
-        totalQuestions[0][15] = "BD: Una base de datos puede ser centralizada o...?\n- Descentralizada\n- Distribuida\n- Fragmentada";
-        totalQuestions[0][16] = "BD: Una base de datos puede ser relacional o...?\n- Individual\n- De entidades\n- No relacional";
-        totalQuestions[0][17] = "BD: En SQL, las instrucciones SELECT, INSERT y DELETE pertenecen al grupo de instrucciones del DML?";
-        totalQuestions[0][18] = "BD: Una tupla es cada una de las filas de una tabla?";
-        totalQuestions[0][19] = "BD: SQL es un SGBD?";
-        totalQuestions[1][0] = "SIS: Cuál es la IP reservada para el localhost?\n- 192.168.0.0\n- 127.0.0.1\n- 1.0.0.0\n- 37434959348385";
-        totalQuestions[1][1] = "SIS: De qué clase es la dirección IP 172.21.240.0?\n- Alfa\n- B\n- Omega\n- C\n- No tiene clase";
-        totalQuestions[1][2] = "SIS: La dirección IP 172.16.255.255 es pública?";
-        totalQuestions[1][3] = "SIS: Al hacer subnetting se amplía el número de equipos que podemos conectar a una misma red?";
-        totalQuestions[1][4] = "SIS: Cuántos equipos podremos conectar a una red con una máscara 255.255.255.0?\n- 256\n- 255\n- 254\n- Ninguno";
-        totalQuestions[1][5] = "SIS: La tercera generación de ordenadores se caracteriza por el microprocesador?";
-        totalQuestions[1][6] = "SIS: Quién definió las partes fundamentales de un ordenador: CPU, memoria principal y unidades de entrada/salida?\n- John von Neumann\n- Charles Babbage\n- Steve Jobs\n- Charles Chapplin";
-        totalQuestions[1][7] = "SIS: Hermann Hollerith es conocido por desencriptar códigos alemanes con la máquina Enigma?";
-        totalQuestions[1][8] = "SIS: Cuántas generaciones de ordenadores hay?\n- 3\n- 4\n- 5\n- 6\n- Demasiadas para ser contadas";
-        totalQuestions[1][9] = "SIS: Una máscara de red 255.255.240.0 es estándar?";
-        totalQuestions[1][10] = "SIS: La dirección IP 10.255.255.255 es privada?";
-        totalQuestions[1][11] = "SIS: En una máscara estándar de clase B pertenecen dos bytes a la porción de host?";
-        totalQuestions[1][12] = "SIS: Cuánto es en decimal el siguiente número binario: 11111111?\n- 128\n- 256\n- 8\n- 64\n- No se puede pasar a decimal";
-        totalQuestions[1][13] = "SIS: Cuántos bits componen un nibble?\n- 2\n- 4\n- 6\n- 8";
-        totalQuestions[1][14] = "SIS: Cómo se llamaba la máquina que usaba Alan Turing?\n- UNIVAC\n- ENIAC\n- ENIGMA\n- CALCULATRÓN 3000";
-        totalQuestions[1][15] = "SIS: Cuál fue el primer ordenador?\n- Apple 2\n- ENIAC\n- R2D2\n- Macintosh 666";
-        totalQuestions[1][16] = "SIS: Dos direcciones IP están en la misma red si tienen la misma máscara de subred?";
-        totalQuestions[1][17] = "SIS: Joseph Marie Jaquard inventó las tarjetas perforadas?";
-        totalQuestions[1][18] = "SIS: Cuántos bytes tiene la dirección 192.168.0.0?\n- 256\n- 4\n- 8\n- 6\n- Depende";
-        totalQuestions[1][19] = "SIS: La dirección de broadcast es la última dirección IP dentro de un rango de red?";
-        totalQuestions[2][0] = "EN: El lenguaje máquina se expresa en código binario?";
-        totalQuestions[2][1] = "EN: Podemos dividir la CPU en dos partes: la CU y la...?\n- SQL\n- DTD\n- ALU\n- RR";
-        totalQuestions[2][2] = "EN: El código ejecutable es el código intermedio entre el código fuente y el código objeto?";
-        totalQuestions[2][3] = "EN: Java tiene un lenguaje propio denominado byte code?";
-        totalQuestions[2][4] = "EN: Qué significan las siglas IDE?\n- Integrated Direction Environment\n- Integrated Development Environment\n- Integrated Definition Environment\n- Inmortals Dying Exclusive";
-        totalQuestions[2][5] = "EN: Un lenguaje puede ser interpretado o...?\n- Traducido\n- Compilado\n- Blasfemo\n- Directo";
-        totalQuestions[2][6] = "EN: Cómo se llama el compilador que emplea la máquina virtual de Java?\n- JAT\n- JET\n- JIT\n- EEUU";
-        totalQuestions[2][7] = "EN: El lenguaje de alto nivel es el que mejor puede entender la máquina?";
-        totalQuestions[2][8] = "LM: Qué significan las siglas JSON?\n- Java System On\n- Javascript Object Notation\n- Java System Open Notes\n- Javascript Open Notes";
-        totalQuestions[2][9] = "LM: Qué significan las siglas XML?\n- Extreme Markup Language\n- Extreme Make-up Language\n- Extreme Manual Language";
-        totalQuestions[2][10] = "LM: Qué significan las siglas HTML?\n- Hambre Tendre Mucha Luego\n- Hit Time Markup Language\n- Hyper Text Markup Language";
-        totalQuestions[2][11] = "LM: Un documento de texto bien formado y uno válido son lo mismo?";
-        totalQuestions[2][12] = "LM: Un documento JSON está compuesto por listas y objetos?";
-        totalQuestions[2][13] = "PR: Un loop while tiene que realizarse al menos una vez?";
-        totalQuestions[2][14] = "PR: Empleamos un bucle for i cuando no conocemos el número de repeticiones?";
-        totalQuestions[2][15] = "PR: Un String es un array de chars?";
-        totalQuestions[2][16] = "PR: Una función 'void' siempre retorna algo?";
-        totalQuestions[2][17] = "LM: La etiqueta <nombre>Manolo<nombre> sería correcta en XML?";
-        totalQuestions[2][18] = "LM: El DTD es un lenguaje de marcas?";
-        totalQuestions[2][19] = "EN: Se puede cambiar el valor de una variable mientras se ejecuta un programa en debug?";
-        totalQuestions[3][0] = "EIE: Cuántos socios tiene que haber como mínimo para montar una Sociedad Civil?\n- 2\n- 3\n- No tiene";
-        totalQuestions[3][1] = "EIE: La Comunidad de Bienes tiene capital mínimo?";
-        totalQuestions[3][2] = "EIE: Una SL tiene un capital mínimo de 3000€?";
-        totalQuestions[3][3] = "EIE: Una SLNE tiene un capital máximo de 60000€?";
-        totalQuestions[3][4] = "EIE: Una SA tiene mínimo de socios?";
-        totalQuestions[3][5] = "EIE: Una SA tiene que desembolsar el 100% del capital mínimo en el momento de su constitución?";
-        totalQuestions[3][6] = "EIE: Una Sociedad Cooperativa tiene responsabilidad ilimitada?";
-        totalQuestions[3][7] = "EIE: Una sociedad colectiva tiene mínimo de socios?";
-        totalQuestions[3][8] = "EIE: En una sociedad laboral, los socios no trabajadores deben tener el 51% del capital mínimo?";
-        totalQuestions[3][9] = "EIE: En una SL los socios pueden vender sus participaciones libremente?";
-        totalQuestions[3][10] = "FOL: Los estibadores portuarios pertenecen a las relaciones laborales especiales?";
-        totalQuestions[3][11] = "FOL: Los transportistas pertenecen a las relaciones laborales especiales?";
-        totalQuestions[3][12] = "FOL: Los convenios colectivos están por encima de los reglamentos en la jerarquía de leyes?";
-        totalQuestions[3][13] = "FOL: Las horas extraordinarias estructurales son obligatorias?";
-        totalQuestions[3][14] = "FOL: El descanso diario de un trabajador ha de ser de mínimo 8 horas?";
-        totalQuestions[3][15] = "FOL: Puede un padre solicitar el permiso de lactancia?";
-        totalQuestions[3][16] = "FOL: Puede un trabajador renunciar a sus vacaciones?";
-        totalQuestions[3][17] = "FOL: Los funcionarios pertenecen a las relaciones laborales especiales?";
-        totalQuestions[3][18] = "FOL: El principio de norma más favorable indica que la norma de rango superior\nestablece las disposiciones mínimas para el trabajador?";
-        totalQuestions[3][19] = "FOL: El principio de condición más beneficiosa indica que si existen dos o más\nnormas que regulan una misma cuestión, se aplicará aquélla que establezca\nlas condiciones más beneficiosas para el trabajador.?";
 
+
+        String [] fileNames = {"src/resources/questions/BDQuestions.txt","src/resources/questions/SISQuestions.txt","src/resources/questions/LM_EN_PRQuestions.txt","src/resources/questions/FOL_EIEQuestions.txt"};
+        BufferedReader br;
+        String line;
+        int y;
+        int x;
+
+        for (y = 0; y < 4; y++) {
+            x=0;
+            try {
+                br = new BufferedReader(new FileReader(fileNames[y]));
+                while ((line = br.readLine()) != null) {
+                    totalQuestions[y][x] = line;
+                    x++;
+                }
+
+            } catch (FileNotFoundException e) {
+
+                System.out.println("NO SE HA ENCONTRADO EL FICHERO");
+
+            } catch (IOException e) {
+
+                System.out.println("ERROR DURANTE LA LECTURA DEL FICHERO");
+            }
+        }
 
     }
 
+
     private static void answersBox(String[][] totalAnswer) {
 
-        totalAnswer[0][0] = "DCL";
-        totalAnswer[0][1] = "NO";
-        totalAnswer[0][2] = "SI";
-        totalAnswer[0][3] = "NO";
-        totalAnswer[0][4] = "SI";
-        totalAnswer[0][5] = "NO";
-        totalAnswer[0][6] = "SI";
-        totalAnswer[0][7] = "NO";
-        totalAnswer[0][8] = "DATA DEFINITION LANGUAGE";
-        totalAnswer[0][9] = "DATA CONTROL LANGUAGE";
-        totalAnswer[0][10] = "DATA MANIPULATION LANGUAGE";
-        totalAnswer[0][11] = "SI";
-        totalAnswer[0][12] = "NO";
-        totalAnswer[0][13] = "SI";
-        totalAnswer[0][14] = "STRUCTURED QUERY LANGUAGE";
-        totalAnswer[0][15] = "DISTRIBUIDA";
-        totalAnswer[0][16] = "NO RELACIONAL";
-        totalAnswer[0][17] = "SI";
-        totalAnswer[0][18] = "SI";
-        totalAnswer[0][19] = "NO";
-        totalAnswer[1][0] = "127.0.0.1";
-        totalAnswer[1][1] = "B";
-        totalAnswer[1][2] = "NO";
-        totalAnswer[1][3] = "NO";
-        totalAnswer[1][4] = "254";
-        totalAnswer[1][5] = "NO";
-        totalAnswer[1][6] = "JOHN VON NEUMANN";
-        totalAnswer[1][7] = "NO";
-        totalAnswer[1][8] = "5";
-        totalAnswer[1][9] = "NO";
-        totalAnswer[1][10] = "SI";
-        totalAnswer[1][11] = "SI";
-        totalAnswer[1][12] = "256";
-        totalAnswer[1][13] = "4";
-        totalAnswer[1][14] = "ENIGMA";
-        totalAnswer[1][15] = "ENIAC";
-        totalAnswer[1][16] = "NO";
-        totalAnswer[1][17] = "SI";
-        totalAnswer[1][18] = "4";
-        totalAnswer[1][19] = "SI";
-        totalAnswer[2][0] = "SI";
-        totalAnswer[2][1] = "ALU";
-        totalAnswer[2][2] = "NO";
-        totalAnswer[2][3] = "SI";
-        totalAnswer[2][4] = "INTEGRATED DEVELOPMENT ENVIRONMENT";
-        totalAnswer[2][5] = "COMPILADO";
-        totalAnswer[2][6] = "JIT";
-        totalAnswer[2][7] = "NO";
-        totalAnswer[2][8] = "JAVASCRIPT OBJECT NOTATION";
-        totalAnswer[2][9] = "EXTREME MARKUP LANGUAGE";
-        totalAnswer[2][10] = "HYPER TEXT MARKUP LANGUAGE";
-        totalAnswer[2][11] = "NO";
-        totalAnswer[2][12] = "SI";
-        totalAnswer[2][13] = "NO";
-        totalAnswer[2][14] = "NO";
-        totalAnswer[2][15] = "SI";
-        totalAnswer[2][16] = "NO";
-        totalAnswer[2][17] = "NO";
-        totalAnswer[2][18] = "NO";
-        totalAnswer[2][19] = "SI";
-        totalAnswer[3][0] = "2";
-        totalAnswer[3][1] = "NO";
-        totalAnswer[3][2] = "SI";
-        totalAnswer[3][3] = "NO";
-        totalAnswer[3][4] = "NO";
-        totalAnswer[3][5] = "NO";
-        totalAnswer[3][6] = "NO";
-        totalAnswer[3][7] = "SI";
-        totalAnswer[3][8] = "NO";
-        totalAnswer[3][9] = "NO";
-        totalAnswer[3][10] = "SI";
-        totalAnswer[3][11] = "NO";
-        totalAnswer[3][12] = "NO";
-        totalAnswer[3][13] = "SI";
-        totalAnswer[3][14] = "NO";
-        totalAnswer[3][15] = "SI";
-        totalAnswer[3][16] = "NO";
-        totalAnswer[3][17] = "NO";
-        totalAnswer[3][18] = "NO";
-        totalAnswer[3][19] = "NO";
+
+        String [] fileNames = {"src/resources/answers/BDAnswers.txt","src/resources/answers/SISAnswers.txt","src/resources/answers/LM_EN_PRAnswers.txt","src/resources/answers/FOL_EIEAnswers.txt"};
+        BufferedReader br;
+        String line;
+        int y;
+        int x;
+
+        for (y = 0; y < 4; y++) {
+            x=0;
+            try {
+                br = new BufferedReader(new FileReader(fileNames[y]));
+                while ((line = br.readLine()) != null) {
+                    totalAnswer[y][x] = line;
+                    x++;
+                }
+
+            } catch (FileNotFoundException e) {
+
+                System.out.println("NO SE HA ENCONTRADO EL FICHERO");
+
+            } catch (IOException e) {
+
+                System.out.println("ERROR DURANTE LA LECTURA DEL FICHERO");
+            }
+        }
+
     }
 }
